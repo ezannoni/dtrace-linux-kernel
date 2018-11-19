1d818eb452fc7e10607b784f1de9fc078f79214c
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-19 22:21:36 +0000

    dtrace: core and x86
    This implements DTrace's core kernel (linked-in) components,
    including platform-dependent portions for x86.  (Most of this
    machinery is not used until the next commit.)
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

---
+++ arch/x86/include/asm/dtrace_util.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
+ */
+
+#ifndef _X86_DTRACE_UTIL_H
+#define _X86_DTRACE_UTIL_H
+
+#ifndef __ASSEMBLY__
+
+#include <asm/dtrace_arch.h>
+#include <asm/ptrace.h>
+
+#endif
+
+#endif /* _X86_DTRACE_UTIL_H */
